82296b39e8a761b666752a24aa621d74f4d7027f
CGearhart <cgearhart@gmail.com>
2016-10-31 17:02:10 -0400

    Update submit script for consistency across assignments

--- submit.py
+++ submit.py
@@ -6,78 +6,101 @@ import json
 import datetime
 from bonnie.submission import Submission
 
-LATE_POLICY = \
-"""Late Policy:
+# open stdout unbuffered to automatically flush prints
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-  \"I have read the late policy for CS6475. I understand that only my last 
-  commit before the late submission deadline will be accepted and that late 
+FILENAMES = {'writeup10': ["assignment10.pdf"]}
+SIZE_LIMIT = 6       # maximum size of each file uploaded
+REFRESH_TIME = 3.0   # number of seconds to wait during results polling
+
+LATE_POLICY = \
+"""Late Policy:\n
+  \"I have read the late policy for CS6475. I understand that only my last
+  commit before the late submission deadline will be accepted and that late
   penalties apply if any part of the assignment is submitted late.\"
 """
 
 HONOR_PLEDGE = "Honor Pledge:\n\n  \"I have neither given nor received aid on this assignment.\"\n"
 
+
 def require_pledges():
-  print(LATE_POLICY)
-  ans = raw_input("Please type 'yes' to agree and continue>")
-  if ans != "yes":
-    raise RuntimeError("Late policy not accepted.")
 
-  print
-  print(HONOR_PLEDGE)
-  ans = raw_input("Please type 'yes' to agree and continue>")
-  if ans != "yes":
-    raise RuntimeError("Honor pledge not accepted")
-  print
+    print(LATE_POLICY)
+    ans = raw_input("Please type 'yes' to agree and continue>")
+    if ans != "yes":
+        raise RuntimeError("You must accept the late policy to submit your assignment.")
+    print
 
-def main():
-  parser = argparse.ArgumentParser(description='Submits code to the Udacity site.')
-  parser.add_argument('--provider', choices = ['gt', 'udacity'], default = 'gt')
-  parser.add_argument('--environment', choices = ['local', 'development', 'staging', 'production'], default = 'production')
+    print(HONOR_PLEDGE)
+    ans = raw_input("Please type 'yes' to agree and continue>")
+    if ans != "yes":
+        raise RuntimeError("You must accept the honor pledge to submit your assignment.")
+    print
 
-  args = parser.parse_args()
 
-  quiz = 'writeup10'
-  filenames = ["assignment10.pdf"]
+def check_files(filenames):
 
-  if not os.path.isfile(filenames[0]):
-    print "%s is not present in the directory." %  filenames[0]
-    return
-  elif (os.stat(filenames[0]).st_size >> 20) >= 6:
-    print "Please keep your files under 6MB."
-    return
+    if not all(map(lambda x: os.path.isfile(x), filenames)):
+        raise RuntimeError("Submission Failed: One or more required files " +
+                           "is missing from the current directory. Make " +
+                           "sure all these files exist: {!s}".format(filenames))
+    elif any(map(lambda x: os.stat(x).st_size >> 20 > SIZE_LIMIT, filenames)):
+        large_files = filter(lambda x: os.stat(x).st_size >> 20 > SIZE_LIMIT, filenames)
+        raise RuntimeError("Submission Failed: One or more required files is " +
+                           "too large. Please make sure that the following " +
+                           "files are under {}MB and try again: {!s}".format(SIZE_LIMIT, large_files))
 
-  require_pledges()
 
-  print "Submission processing...\n"
-  submission = Submission('cs6475', quiz, 
-                          filenames = filenames, 
-                          environment = args.environment, 
-                          provider = args.provider)
+def main(args):
 
-  timestamp = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
+    check_files(args.filenames)
 
-  while not submission.poll():
-    time.sleep(3.0)
+    require_pledges()
 
-  if submission.feedback():
+    print "Submitting files..."
+    submission = Submission('cs6475', args.quiz,
+                            filenames=args.filenames,
+                            environment=args.environment,
+                            provider=args.provider)
 
-    if submission.console():
-        sys.stdout.write(submission.console())
+    print "\nWaiting for results..."
+    while not submission.poll():
+        print "    Refresh in {} seconds...".format(REFRESH_TIME)
+        time.sleep(REFRESH_TIME)
+    print "    Done!"
 
-    filename = "%s-result-%s.json" % (quiz, timestamp)
+    print "\nResults:\n--------"
+    if submission.feedback():
 
-    with open(filename, "w") as fd:
-      json.dump(submission.feedback(), fd, indent=4, separators=(',', ': '))
+        if submission.console():
+                print submission.console()
 
-    print("\n(Details available in %s.)" % filename)
+        timestamp = "{:%Y-%m-%d-%H-%M-%S}".format(datetime.datetime.now())
+        filename = "%s-result-%s.json" % (args.quiz, timestamp)
 
-  elif submission.error_report():
-    error_report = submission.error_report()
-    print(json.dumps(error_report, indent=4))
-  else:
-    print("Unknown error.")
+        with open(filename, "w") as fd:
+            json.dump(submission.feedback(), fd, indent=4, separators=(',', ': '))
+
+        print("\n(Details available in %s)\n" % filename)
+
+    elif submission.error_report():
+        error_report = submission.error_report()
+        print(json.dumps(error_report, indent=4))
+
+    else:
+        print("Unknown error.")
+
 
 if __name__ == '__main__':
-  main()
+    parser = argparse.ArgumentParser(description='Submits code to the Udacity site.')
+    parser.add_argument('quiz', choices=FILENAMES.keys(),
+                        help="select the name of the quiz to submit")
+    parser.add_argument('--provider', choices=['gt', 'udacity'], default='gt',
+                        help="select the authentication system to use (default: gt)")
+    parser.add_argument('--environment', default='production',
+                        choices=['local', 'development', 'staging', 'production'],
+                        help="select the server to use (default: production)")
+    args = parser.parse_args()
+    setattr(args, 'filenames', FILENAMES[args.quiz])
 
-
+    main(args)
